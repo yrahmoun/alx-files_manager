f3404b6d79362bebbb890091c5f3295f2df628f5
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-08-03 02:51:28 -0700

    update

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
+import sha1 from 'sha1';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
-import sha1 from 'sha1';
 
 class AuthController {
   static async getConnect(req, res) {
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,6 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import sha1 from 'sha1';
 
 class UsersController {
   static async postNew(req, res) {
